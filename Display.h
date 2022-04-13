f25e1afc43aa612e845f40b8aabcb44c7c8460c1
tiendung050803 <tiendung050803@gmail.com>
2022-04-13 23:08:01 +0700

    Create Display.h

---
+++ Display.h
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#define ii pair<int,int>
+#define ll long long
+using namespace std;
+
+void displayWon();
+void displayLoose(const string& secretWord);
+void clearScreen();
