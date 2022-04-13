44eadfcb5595ac40f5750fa3aa2068f958071677
tiendung050803 <tiendung050803@gmail.com>
2022-04-13 20:32:04 +0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ii pair<int,int>
+#define ll long long
+using namespace std;
+const int mod = 1e9+7;
+const int base = 331;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	string line;
+	ifstream myfile ("dataword.txt");
+	if(myfile.is_open())
+	{
+		while(getline(myfile,line))
+		{
+			
+		}
+		myfile.close();
+	}
+	
+}
