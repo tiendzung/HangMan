5381bdba17e877a21853b54016573004cfcf947a
tiendung050803 <tiendung050803@gmail.com>
2022-04-13 18:38:46 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ii pair<int,int>
+#define ll long long
+using namespace std;
+const int mod = 1e9+7;
+const int base = 331;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	
+}
